6d050a64158d5d23653f4316acca02f85f2d1c02
srwheeler <scot.wheeler@gmail.com>
2015-04-11 22:33:24 -0500

    Assignment 2 Index File
    Signed-off-by: srwheeler <scot.wheeler@gmail.com>

--- index_assignment2.php
+++ index_assignment2.php
@@ -184,7 +184,7 @@ function getCookie(cname) {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-46392789-1', 'auto');
+  ga('create', 'UA-11111111-1', 'auto');
   ga('send', 'pageview');
 
 </script>
